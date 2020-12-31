ecda2200603d3c7678077bec485efc8044ddb3a1
James Darby <56593075+Darbjm@users.noreply.github.com>
2020-12-31 17:44:22 +0000

    delete

--- Maps/main.go
+++ Maps/main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
 
-	colors := make(map[int]string)
+	// colors := make(map[int]string)
 
-	colors[10] = "#ffffff"
+	// colors[10] = "#ffffff"
 
-	delete(colors, 10)
+	// delete(colors, 10)
 
 	fmt.Println(colors)
 }
