6cd8aa318ce3500e36c43cddf402b4f6e7cc764a
James Darby <56593075+Darbjm@users.noreply.github.com>
2020-12-31 17:47:35 +0000

    iterating over maps

--- Maps/main.go
+++ Maps/main.go
@@ -6,6 +6,7 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 
 	// colors := make(map[int]string)
@@ -14,5 +15,13 @@ func main() {
 
 	// delete(colors, 10)
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
